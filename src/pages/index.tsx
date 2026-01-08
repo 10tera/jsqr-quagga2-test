2636c49a93d713f2213e4ba7221ea84c8ea908eb
YutoTanimura <yu_tanimura@yumemi.co.jp>
2026-01-08 12:41:47 +0900

    fix: code39 link text

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       <h1>jsQRとquagga2のテストアプリ</h1>
       <Link href="/jsqr">QRコード読み取り（jsQR）</Link>
       <br />
-      <Link href="/quagga2">バーコード読み取り（quagga2）</Link>
+      <Link href="/quagga2">バーコード読み取り（quagga2 with code39/vin）</Link>
       <br />
       <Link href="/quagga2-code-128">
         バーコード読み取り（quagga2 with code128）
